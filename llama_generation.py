55c03648681e7ce81ed42b294bd4b1927858a444
ShaoshuSu <shaoshusu@gmail.com>
2024-07-16 00:39:39 -0400

    update the instruction text for professorship

--- llama_generation.py
+++ llama_generation.py
@@ -42,16 +42,20 @@ class LLAMA_Generation:
         self.max_gen_len = None
         self.instructions_text = "You are tasked with analyzing the information provided to determine" \
             "if the individual in question holds the position of a professor." \
-            "Use your analytical capabilities to review the details given and make a judgment " \
             "based on the information available." \
             "Please provide your response in the following format: 'YES', 'NO', or 'NOT SURE'." \
-            # "For example, the following is not a professor:" \
+            "You can answer 'YES' only when that text mentions that the individual is a professor." \
+            "You can answer 'YES' only when that text has direct confirmation of professorship" \
+            "Researcher is not a direct confirmation of professorship." 
+            # "And provide me a score of your confidence for if that person is a professor." \
+            # "Use your analytical capabilities to review the details given and make a judgment " \
+            # "\nFor example, for the following, the answer is 'NO': " \
             # "Changhwan Kim (Member, IEEE) received the B.S. degree in mechanical engineering and the M.S. degree in machine design engineering from Hanyang University, Seoul, South Korea, in 1993 and 1995, respectively, and the Ph.D. degree in mechanical engineering from The University of Iowa, Iowa City, IA, USA, in 2002. From 2002 to 2004, he was a Research Associate with the Robotics and Automation Laboratory, University of Notre Dame, Notre Dame, IN, USA. Since 2004, he has been working at the Korea Institute of Science and Technology (KIST), Seoul. His research interests include task and motion planning for robot manipulation and social robots.(Based on document published on 26 December 2022)." \
-            # "For another example, the following is not a professor:" \
+            # "\nFor another example, the following is not a professor:" \
             # "Younbaek Lee received the M.S. degree in mechanical engineering from Korea Advanced Institute of Science and Technology, Daejeon, South Korea, in 2003.,He is currently a research staff member of the Samsung Advanced Institute of Technology, South Korea. His research interests include electromechanical design, medical/rehabilitation robotics, humanoids, manipulators and physical human-machine interaction.(Based on document published on 14 June 2019)." \
-            # "For one more example, the following is not a professor:" \
+            # "\nFor one more example, the following is not a professor: " \
             # "Shenghai Yuan received the bachelor’s and Ph.D. degrees in electrical and electronic engineering from the Nanyang Technological University, Singapore, in 2013 and 2019, respectively.,He is a Research Fellow with the EEE Internet of Things Lab, Nanyang Technological University. His research interests include the area of perception, sensor fusion for robust navigation, machine learning, and autonomous system.(Based on document published on 15 August 2023)." \
-            # "For the third example, the following is a professor:" \
+            # "\nFor the fourth example, the following is a professor: " \
             # "Guyue Zhou (Member, IEEE) received the B.E. degree from the Harbin Institute of Technology, Harbin, China, in 2010, and the Ph.D. degree from the Hong Kong University of Science and Technology, Hong Kong, China, in 2014. He is currently an Associate Professor with the Institute for AI Industry Research (AIR), Tsinghua University. His research interests include advanced manufacturing, robotics, computer vision, and human–machine interaction.(Based on document published on 22 August 2022)."
 
 
@@ -99,12 +103,14 @@ class LLAMA_Generation:
                 )
                 print("\n==================================\n")
 
-        if "YES" in result['generation']['content']:
+        if "not sure" in result['generation']['content'].lower():
+            return "NOT SURE", result['generation']['content']
+        if "yes" in result['generation']['content'].lower():
             return "YES", result['generation']['content']
-        elif "NO" in result['generation']['content']:
+        elif "no" in result['generation']['content'].lower():
             return "NO", result['generation']['content']
         else:
-            return "NOT SURE", result['generation']['content']
+            return None, None
 
         # return result['generation']['content']
     
